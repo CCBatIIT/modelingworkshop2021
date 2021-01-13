e23090bcf4fed4789dd94e682c96003fab36b9d1
David Minh <daveminh@gmail.com>
2021-01-13 05:24:37 -0600

    MPro trajectory loader

---
+++ exercises/mpro/RIKEN_trajectory/get_trajectory.sh
@@ -0,0 +1,3 @@
+# https://covid.molssi.org//simulations/#riken-bdr-10-microsecond-trajectory-protein-snapshot-every-1ns
+wget https://covmme.molssi.org/data/rikenBDR/rikenBDR_trajs_0/main_protease/rikenBDR-Trajectory_sarscov2-10921231-structure/sarscov2-10921231-structure.tar
+wget https://covmme.molssi.org/data/rikenBDR/rikenBDR_trajs_0/main_protease/rikenBDR-Trajectory_sarscov2-10921231-trajectory/sarscov2-10921231-protein/traj_protein_snap_every1ns.tar
